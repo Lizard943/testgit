fddc6c50af396b7ab75e096aae64339c7ca65de9
Lizard943 <vuliz943@gmail.com>
2025-08-27 10:38:34 +0700

    feat: create obj

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 // let có thể gán lại
-let a = 10
-a = 5 // OK
+let a = 10;
+a = 5; // OK
 
 // const không thể gán lại
-const b = 2
-b = 6 // Error
+const b = 2;
+b = 6; // Error
 
 // Với obj
-const obj = { name: 'Luân'};
-obj.name = 'Dũng'
+const obj = { name: "Luân" };
+obj.name = "Dũng";
 // obj = { name: 'Dũng' } // Error
+
+const oo = { name: "Dũng" };
